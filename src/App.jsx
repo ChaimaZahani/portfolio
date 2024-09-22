459b43940df07dcc80312fc6ca62848770025918
Chaima Zahani <chaima.zahani@enis.tn>
2024-09-22 18:45:59 +0100

    portfolio

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "./sections/Navbar.jsx";
+import Hero from "./sections/Hero.jsx";
+import About from "./sections/About.jsx";
+import Projects from "./sections/Projects.jsx";
+import Contact from "./sections/Contact.jsx";
+import WorkExperience from "./sections/Experience.jsx";
+import Footer from "./sections/Footer.jsx";
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto">
+      <Navbar />
+
+      <Hero />
+      <About />
+      <Projects />
+      <WorkExperience />
+      <Contact />
+      <Footer />
+    </main>
+  );
+};
+
+export default App;
